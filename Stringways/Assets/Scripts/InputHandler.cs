7dbba2adf993d6f33d12cb80f526c1f7dbb0eeb2
longzac10 <138516322+longzac10@users.noreply.github.com>
2024-05-12 22:47:37 +1000

    Created Prefab for String

--- Stringways/Assets/Scripts/InputHandler.cs
+++ Stringways/Assets/Scripts/InputHandler.cs
@@ -10,22 +10,24 @@ public class InputHandler : MonoBehaviour
     public LineRenderer lineRenderer;
     public GameObject firstObject;
     public GameObject secondObject;
+    public GameObject newString;
     public Material lineMaterial; // Material for the line
     public float lineWidth = 0.1f; // Width of the line
     private bool isDrawing = false;
     private bool click = false;
     #endregion
 
-    private void Awake()
+    void Start()
     {
         _mainCamera = Camera.main;
         lineRenderer.positionCount = 2;
+        Instantiate(newString, new Vector3(0, 0, 0), Quaternion.identity);
     }
 
     void Update()
     {
-      
-        if(isDrawing)
+
+        if (isDrawing)
         {
             Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             lineRenderer.SetPosition(0, firstObject.transform.position);
@@ -33,26 +35,21 @@ public class InputHandler : MonoBehaviour
         }
         else
         {
-            if(click)
+            if (click)
             {
                 lineRenderer.SetPosition(0, firstObject.transform.position);
                 lineRenderer.SetPosition(1, secondObject.transform.position);
             }
-            
+
         }
     }
 
-    public void OnClick(InputAction.CallbackContext context)
+    private void OnClick()
     {
-        if (!context.started) return;
-
-
-        
         var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
-        if (!rayHit.collider) return;
-        
 
-        if(rayHit.collider.gameObject.tag == "Town" && isDrawing ==false)
+
+        if (rayHit.collider.gameObject.tag == "Town" && isDrawing == false)
         {
             isDrawing = true;
             GameObject line = new GameObject("Line"); // Create a new GameObject for the line
@@ -64,14 +61,30 @@ public class InputHandler : MonoBehaviour
         }
         else
         {
-            if(rayHit.collider.gameObject.tag == "Town")
+            if (rayHit.collider.gameObject.tag == "Town")
             {
                 secondObject = rayHit.collider.gameObject;
                 isDrawing = false;
             }
-            
+
         }
         click = true;
         Debug.Log(rayHit.collider.gameObject.name);
+    }
+
+    private void OnRightClick()
+    {
+        // Create a ray from the camera to the mouse position
+        var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+        //RaycastHit hit;
+
+        // Check if the object hit by the ray has a LineRenderer component
+        LineRenderer lineRenderer = rayHit.collider.gameObject.GetComponent<LineRenderer>();
+        if (lineRenderer != null)
+        {
+            // If so, destroy the object
+            Destroy(rayHit.collider.gameObject);
+        }
+        Debug.Log("Right click");
     }
 }
